49608ec8073b061c25dcc940ab262f7a84f1f71d
StarryNightSun <sasisc914@gmail.com>
2017-01-21 17:32:10 -0500

    Updated JS, Tried to identify fb posts

---
+++ GarbPostCleanerV1/bootstrap.js
@@ -0,0 +1,11 @@
+// when the extension is first installed
+chrome.runtime.onInstalled.addListener(function(details) {
+    chrome.storage.sync.set({clean_news_feed: true});
+});
+
+// listen for any changes to the URL of any tab.
+chrome.tabs.onUpdated.addListener(function(id, info, tab){
+    if (tab.url.toLowerCase().indexOf("facebook.com") > -1){
+        chrome.pageAction.show(tab.id);
+    }
+});
--- GarbPostCleanerV1/content.js
+++ GarbPostCleanerV1/content.js
@@ -1,2 +1,12 @@
 // content.js
-alert("Hello from your Chrome extension!")
+smash();
+
+function smash(){
+  alert("Hello from your Chrome extension!")
+  var posts = document.getElementsByClassName("uiStreamMessage");
+  alert(posts);
+  for(var i=0; i<posts.length; i++){
+    if(posts[i].innerHTML.indexOf("orange") != -1){
+      posts[i].parentNode.removeChild(posts[i])};
+    }
+  }
