488d2b5a20e74c294e520b387448eedbd3ac9ebe
Maxwell Chan <Maxwell Chan>
2017-01-21 20:44:40 -0500

    Optimized some code

--- GarbPostCleanerV1/compare.js
+++ GarbPostCleanerV1/compare.js
@@ -2,20 +2,19 @@
 function filter(post, blacklist) {
 //dictionary of sloth facts
 var dict = [
-  "1: There are two types of sloths: the two-toed sloth and the three-toed sloth",
-"2: Sloths are closely related to anteaters and armadillos", "3: Sloths move slow enough for algae and various creatures to live in their fur, turning them into mini-ecosystems"
+  "There are two types of sloths: the two-toed sloth and the three-toed sloth.",
+"Sloths are closely related to anteaters and armadillos.", "Sloths move slow enough for algae and various creatures to live in their fur, turning them into mini-ecosystems."
 ];
 
 var message_number = Math.floor(Math.random() * (dict.length));
 
-var n = post.includes(blacklist);
-if (n===true) {
+if (post.includes(blacklist)) {
 	return dict[message_number];
-}else{
-return post;
 }
+
+return post;
 }
 
 var ay = "i eat bean";
-var lmao = "bean";
+var lmao = "ass";
 process.stdout.write(filter(ay, lmao));
