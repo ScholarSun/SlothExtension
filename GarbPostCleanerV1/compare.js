50d42bb4e3dad0c7587fce7d93b4a357b3299b7e
Maxwell Chan <Maxwell Chan>
2017-01-21 23:41:15 -0500

    Optimized some code and added more sloth facts

--- GarbPostCleanerV1/compare.js
+++ GarbPostCleanerV1/compare.js
@@ -5,16 +5,18 @@ var dict = [
   "Sloth tongues can extend 10 - 12 inches out of their mouths", "There are two types of sloths: the two-toed sloth and the three-toed sloth.",
 "Sloths are closely related to anteaters and armadillos.", "Sloths move slow enough for algae and various creatures to live in their fur, turning them into mini-ecosystems.", "Sloths rarely climb down from the trees they live in unless they need to take their weekly dump.", "Two-toed sloths are nocturnal while Three-toed sloths are diurnal.", "Wild sloths live for around 10 - 16 years while sloths in captivity can live up to 30 years old.", "Sloths sleep for around 10 hours a day.", "Sloths can swim.", "Some extinct varieties of sloths used to be as big as elephants."
 ];
-
+// generate a random sloth fact
 var message_number = Math.floor(Math.random() * (dict.length));
 
-if (post.includes(blacklist)) {
+word_index = blacklist.indexOf(blacklist)
+
+if (post.toLowerCase().includes(blacklist.toLowerCase())) {
 	return dict[message_number];
 }
 
 return post;
 }
 
-var ay = "i eat bean";
-var lmao = "bean";
+var ay = "i eatbean";
+var lmao = "Bean";
 process.stdout.write(filter(ay, lmao));
