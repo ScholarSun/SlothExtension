ebb4c90344e7219ef07fb6359e5fc24ee949a977
StarryNightSun <sasisc914@gmail.com>
2017-01-22 05:40:41 -0500

    Deletes paragraph posts, started on inserting text

--- GarbPostCleanerV1/content.js
+++ GarbPostCleanerV1/content.js
@@ -7,17 +7,34 @@ document.addEventListener("scroll", scroll);
 
 function getpost(){
   var suck = [];
-  var all = document.getElementsByClassName("_5jmm");
+
   var conten = document.getElementsByTagName("P");
-  for (var i=0, max=conten.length; i < max; i++) {
+  var divs = document.getElementsByClassName('_5pbx');
+
+  var div;
+  var i = divs.length;
+
+  while (i--) {
+    div = divs[i];
+    div.parentNode.removeChild(div);
+    var node = document.createElement("P");                 // Create a <li> node
+    var textnode = document.createTextNode("Water bruh yo");         // Create a text node
+    node.appendChild(textnode);
+    console.log(node.appendChild(textnode));
+    //div.parentNode.appendChild(node);                              // Append the text to <li>
+    //document.getElementByClassName("_5pbx").appendChild(node);
+
+  }
+
+  /*for (var i=0, max=conten.length; i < max; i++) {
     console.log(conten.item(i));
     if(_.contains(suck,conten)===false){
       suck.push(conten);
     }
-  }
+  }*/
   console.log("BREAK HERE");
   for(var i=0, max=suck.length; i < max; i++){
-    console.log(suck[i]);
+  console.log(suck[i]);
   }
 }
 
@@ -29,4 +46,8 @@ function smash(){
     if(posts[i].innerHTML.indexOf("orange") != -1){
       posts[i].parentNode.removeChild(posts[i])};
     }
+  }
+
+  function replace(){
+
   }
